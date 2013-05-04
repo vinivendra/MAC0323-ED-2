393ba83234bd0ea63873cc49b33832ecf513f011
Vinicius Vendramini <vinivendra@gmail.com>
2013-05-04 09:36:44 -0300

    Includes nos Distances. Adicionado o vetor global distancias a Distance.c.

--- ED2/Distance.c
+++ ED2/Distance.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "Distance.h"
+
+/*
+ Defines e variáveis globais
+    Será usado um vetor de distâncias, que é a parte principal do algoritmo que calcula o limiar de conexidade. Ele deve ser inicializado, ordenado e então deve-se buscar a distância desejada, que é uma aproximação do limiar.
+*/
+
+static float *distancias;
